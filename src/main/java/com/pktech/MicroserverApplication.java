3a0c2dd0b71c529c3cf5eb5f6b35c9676a0a4ea3
wangbaolong-pk <529309618@qq.com>
2018-07-07 14:04:12 +0800

    后台代码

---
+++ src/main/java/com/pktech/MicroserverApplication.java
@@ -0,0 +1,13 @@
+package com.pktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserverApplication.class, args);
+	}
+}
+
---
+++ src/main/java/com/pktech/controller/controller.java
@@ -0,0 +1,38 @@
+package com.pktech.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pktech.entity.User;
+import com.pktech.service.UserService;
+
+@RestController
+public class controller {
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	
+	public Map<String,Object> login(@RequestParam String phone,@RequestParam String password){
+		Map<String,Object> map=new HashMap<String,Object>();
+		User user=new User();
+		user=userService.findUserByPhone(phone);
+		
+			if(password.equals(user.getPassword())) {
+				map.put("0000", "登陆成功");
+			}else {
+				map.put("0001", "密码错误");
+			}
+			
+		
+		
+		return map;
+	}
+}
